e91011e83b13683d9df1eda9ceee6874f8e20ff9
eren-dox <eren.doksatli@gmail.com>
2024-03-31 14:30:55 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+import { renderMenuItems, renderButtons } from "./scripts/ui.js";
+
+const menuList = document.querySelector("#menu-list");
+const buttonsArea = document.getElementById("buttons");
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderButtons();
+  fetchMenu();
+});
+
+let data;
+
+async function fetchMenu() {
+  const res = await fetch("./db.json");
+  data = await res.json();
+  renderMenuItems(data.menu, menuList);
+}
+
+buttonsArea.addEventListener("click", (e) => {
+  if (e.target.id !== "buttons") {
+    renderButtons(e.target.innerText);
+    const selected = e.target.dataset.category;
+
+    if (selected === "all") {
+      renderMenuItems(data.menu, menuList);
+    } else {
+      const filtred = data.menu.filter((i) => i.category === selected);
+      renderMenuItems(filtred, menuList);
+    }
+  }
+});
